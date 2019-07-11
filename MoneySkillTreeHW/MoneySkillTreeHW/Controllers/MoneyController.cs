3591be1aef03c4ad348010a18cff3c719b3b71c7
Sharon Chen <sharonsky1217@gmail.com>
2019-07-11 13:48:36 +0800

    完成記帳本的Detail(假資料)與整體畫面

---
+++ MoneySkillTreeHW/MoneySkillTreeHW/Controllers/MoneyController.cs
@@ -0,0 +1,52 @@
+﻿using MoneySkillTreeHW.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoneySkillTreeHW.Controllers
+{
+    public class MoneyController : Controller
+    {
+        // GET: Money
+        public ActionResult MyAccount()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult MyAccount(MoneyViewModel pAccData)
+        {
+            return View(pAccData);
+        }
+
+        /// <summary>
+        /// 下方資料列Action
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult MyAccountDetail()
+        {
+            List<MoneyViewModel> moneyList = new List<MoneyViewModel>();
+
+            #region 取得假資料
+            DateTime firstDate = new DateTime(2019, 1, 1);
+            MoneyViewModel tmpMoney;
+            Random radm = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                tmpMoney = new MoneyViewModel()
+                {
+                    Type = radm.Next(1, 1000) % 2 == 1 ? "支出" : "收入",
+                    MoneyDate = firstDate.AddDays(i),
+                    Amt = radm.Next(1, 50000)
+                };
+                moneyList.Add(tmpMoney);
+            }
+            #endregion
+
+            return View(moneyList);
+        }
+
+    }
+}
---
+++ MoneySkillTreeHW/MoneySkillTreeHW/Models/ViewModels/MoneyViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace MoneySkillTreeHW.Models.ViewModels
+{
+    public class MoneyViewModel
+    {
+        /// <summary>
+        /// 種類
+        /// </summary>
+        [DisplayName("類別")]
+        public string Type { get; set; }
+        /// <summary>
+        /// 支出、收入日期
+        /// </summary>
+        [DisplayName("日期")]
+        public DateTime MoneyDate { get; set; }
+        /// <summary>
+        /// 支出、收入金額
+        /// </summary>
+        [DisplayName("金額")]
+        public double Amt { get; set; }
+    }
+}
