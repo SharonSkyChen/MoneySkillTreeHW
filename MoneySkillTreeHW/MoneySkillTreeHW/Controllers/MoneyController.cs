87fb29901fcbac62b4f6c35e2003b84e0d1e73c7
Sharon Chen <sharonsky1217@gmail.com>
2019-07-15 20:57:32 +0800

    改由DB讀取資料(使用EntityFramework)

--- MoneySkillTreeHW/MoneySkillTreeHW/Controllers/MoneyController.cs
+++ MoneySkillTreeHW/MoneySkillTreeHW/Controllers/MoneyController.cs
@@ -1,4 +1,5 @@
-﻿using MoneySkillTreeHW.Models.ViewModels;
+﻿using MoneySkillTreeHW.Models;
+using MoneySkillTreeHW.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,7 @@ namespace MoneySkillTreeHW.Controllers
 {
     public class MoneyController : Controller
     {
+        AccountBookContext accBookDB = new AccountBookContext();
         // GET: Money
         public ActionResult MyAccount()
         {
@@ -29,20 +31,34 @@ namespace MoneySkillTreeHW.Controllers
         {
             List<MoneyViewModel> moneyList = new List<MoneyViewModel>();
 
-            #region 取得假資料
-            DateTime firstDate = new DateTime(2019, 1, 1);
-            MoneyViewModel tmpMoney;
-            Random radm = new Random();
-            for (int i = 0; i < 100; i++)
+            #region 取得資料庫資料
+            var accBookList = accBookDB.AccountBooks.Take(100).OrderBy(d => d.Dateee).ToList();
+
+            foreach (var book in accBookList)
             {
-                tmpMoney = new MoneyViewModel()
+                moneyList.Add(new MoneyViewModel()
                 {
-                    Type = radm.Next(1, 1000) % 2 == 1 ? "支出" : "收入",
-                    MoneyDate = firstDate.AddDays(i),
-                    Amt = radm.Next(1, 50000)
-                };
-                moneyList.Add(tmpMoney);
+                    Type = book.Categoryyy == 1 ? "支出" : "收入",
+                    MoneyDate = book.Dateee,
+                    Amt = book.Amounttt
+                });
             }
+            #endregion
+
+            #region 取得假資料 註解
+            //DateTime firstDate = new DateTime(2019, 1, 1);
+            //MoneyViewModel tmpMoney;
+            //Random radm = new Random();
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    tmpMoney = new MoneyViewModel()
+            //    {
+            //        Type = radm.Next(1, 1000) % 2 == 1 ? "支出" : "收入",
+            //        MoneyDate = firstDate.AddDays(i),
+            //        Amt = radm.Next(1, 50000)
+            //    };
+            //    moneyList.Add(tmpMoney);
+            //}
             #endregion
 
             return View(moneyList);
